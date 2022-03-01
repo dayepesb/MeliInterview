ff13379d8d992122076cb4417cd655426b52f0e2
David Yepes <davidyepesb@gmail.com>
2022-03-01 03:28:00 -0500

    Configure state redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/configureStore';
+import App from './routes/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+import {
+  SET_USER,
+} from '../utils/ActionsUtils';
+
+const INITIAL_STATE = {
+  user: {},
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case SET_USER:
+      return {
+        ...state,
+        user: action.payload,
+      };
+    default:
+      return state;
+  }
+};
